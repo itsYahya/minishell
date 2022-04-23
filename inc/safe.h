db9bba50088f9a9ac6d0d24fee04f639295f6247
Yahya El mrabti <yel-mrab@e1r4p11.1337.ma>
2022-04-23 03:11:33 +0000

    clear

--- inc/safe.h
+++
@@ -1,5 +0,0 @@
-#ifndef SAFE_H
-# define SAFE_H
-
-int	ft_perror(char *msg);
-#endif
--- src/execution/builtins/pwd.c
+++
@@ -1,9 +0,0 @@
-#include "builtins.h"
-#include <stdio.h>
-#include <unistd.h>
-
-void	ft_pwd(t_context *ctx)
-{
-	ft_putstr(1, ctx->cwd);
-	ft_putstr(1, "\n");
-}
